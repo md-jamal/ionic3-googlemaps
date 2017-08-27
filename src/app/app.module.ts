8fc2a83a865c46525b335055f6e7b7b0133c0f5f
md-jamal <md.jamalmohiuddin@gmail.com>
2017-08-27 16:30:15 +0530

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+import { AngularFireModule } from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+const firebaseAuth = {
+  apiKey: "AIzaSyAaHxOR1cTE9DAdWIfEtXMRisPAcVYGDx0",
+  authDomain: "hyderabadlabs-74d7e.firebaseapp.com",
+  databaseURL: "https://hyderabadlabs-74d7e.firebaseio.com",
+  projectId: "hyderabadlabs-74d7e",
+  storageBucket: "hyderabadlabs-74d7e.appspot.com",
+  messagingSenderId: "115657912938"
+};
+
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +40,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { Geolocation } from '@ionic-native/geolocation';
+import { AngularFireDatabase , FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+
+
+declare var google;
+declare var latitude;
+declare var longitude;
+
+export  interface GPS
+{
+  latitude:string;
+  longitude:string;
+}
 
 @Component({
   selector: 'page-home',
@@ -7,8 +21,120 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  marker:any;
+  
+  
+  constructor(public afd:AngularFireDatabase,private geolocation: Geolocation, public navCtrl: NavController) {
 
   }
 
+  initMap() {
+
+    const image = {
+      url: 'file:///android_asset/www/assets/icon/car.png',
+      size: {
+        width: 24,
+        height: 24
+      }
+    };
+    
+
+    this.geolocation.getCurrentPosition().then((position) => {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      console.log("Latitude"+latitude);
+      console.log("Longitude"+longitude);
+      //longitude = this.firebaseProvider.getLongitude();
+      //console.log("Longitude"+longitude);
+      let latLng = new google.maps.LatLng(latitude, longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,     
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);     
+      let marker = new google.maps.Marker({
+        position: latLng,
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        icon: {
+          url: 'file:///android_asset/www/assets/icon/car.png'        
+          
+        }
+		 });
+    });
+  }
+
+  addInfoWindow(marker, content) {
+    
+        let infoWindow = new google.maps.InfoWindow({
+          content: content
+        });
+    
+        google.maps.event.addListener(marker, 'click', () => {
+          infoWindow.open(this.map, marker);
+        });
+    
+      }
+    
+
+  showMyLocation() 
+  {
+    console.log("showmylocation");
+   
+    this.geolocation.getCurrentPosition().then((position) => {
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      console.log("Latitude"+latitude);
+      console.log("Longitude"+longitude);
+      let latLng = new google.maps.LatLng(latitude, longitude);  
+      let marker = new google.maps.Marker({
+        position: latLng,
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+      });
+    });
+  }    
+
+
+
+  clearMarkers()
+  {
+    //this.marker.setMap(null);
+  }
+
+
+  addMarker(location)
+  {
+     this.marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+    });
+  }
+
+  getMyLocation()
+  {
+    this.clearMarkers();
+    var ref = this.afd.database.ref('/cabs/cabs1/geolocation');
+    ref.on("value", (snapshot) =>   {
+      var test = snapshot.val();
+        console.log("Latitude"+test.latitude);
+        let latLng = new google.maps.LatLng(test.latitude, test.longitude);  
+        this.addMarker(latLng);
+       
+    });
+
+
+  }
+
+
+  ionViewDidLoad(){
+    this.initMap();
+  }
 }
